894bb077e463c71b1d142d65a9d04681ba5450fa
Giorgi Kakabadze <giokakabadze50@gmail.com>
2023-05-12 16:45:35 +0400

    Simple landing page with animations

---
+++ tailwind.config.cjs
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: "class",
+	theme: {
+		extend: {
+			animation: {
+				blob: "blob 7s infinite",
+			},
+			keyframes: {
+				blob: {
+					"0%": {
+						transform: "translate(0px, 0px) scale(1)",
+					},
+					"33%": {
+						transform: "translate(30px, -50px) scale(1.1)",
+					},
+					"66%": {
+						transform: "translate(-20px, 20px) scale(0.9)",
+					},
+					"100%": {
+						transform: "tranlate(0px, 0px) scale(1)",
+					},
+				},
+			},
+			colors: {
+				body: "rgb(var(--color-bg))",
+				"box-bg": "rgb(var(--color-box))",
+				"box-shadow": "rgb(var(--box-sd))",
+				"box-border": "rgb(var(--box-border))",
+				primary: "#1d4ed8",
+				"heading-1": "rgb(var(--heading-1))",
+				"heading-2": "rgb(var(--heading-2))",
+				"heading-3": "rgb(var(--heading-3))",
+			},
+			screens:{
+				midmd:"880px"
+			}
+		},
+	},
+	plugins: [],
+}
